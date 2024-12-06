754e940d7f7879c8b425746cea690b37b21f5c64
d3haka <arian.rassaee@gmail.com>
2024-12-06 22:52:10 +0330

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+const repo = "four-card-feature-section-master";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 export default nextConfig;
