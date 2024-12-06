5573b4114d3185886743c4c5ad48ba71608c00e7
d3haka <arian.rassaee@gmail.com>
2024-12-06 22:43:38 +0330

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="text-center w-full text-3xl text-blue-500 font-bold tracking-widest py-10">
+          ayo hello world
+        </div>
         <Image
           className="dark:invert"
           src="/next.svg"
